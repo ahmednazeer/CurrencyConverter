817e9045602ca96e0776861e8d086c42603a153d
Ahmed_Nazeer <ahmedgelany45@gmail.com>
2025-05-17 12:26:47 +0300

    add core models and contracts

---
+++ CurrencyConverter.Core/Models/ExchangeRate.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CurrencyConverter.Core.Models
+{
+    public class ExchangeRate
+    {
+        public string BaseCurrency { get; set; }
+        public string TargetCurrency { get; set; }
+        public decimal Rate { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
